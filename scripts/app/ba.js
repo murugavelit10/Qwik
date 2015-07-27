49b0053083af60657ac8d317bead5dbfcb9597f1
unknown <gbs03602@SVILP211.SG.GBS.PRO>
2015-07-27 15:18:23 +0530

    show labels, add label, edit label, delete label, show label details implemented.

--- scripts/app/ba.js
+++ scripts/app/ba.js
@@ -15,11 +15,11 @@
 				e.preventDefault();
 				$('.labelDetails').fadeOut(addLabelDetailCb);
 			});
-			$('.label, .labelDetail').on('mouseenter', _.bind(function(e){
-				$(e.currentTarget).find('.actions').show();
-			}, this)).on('mouseleave', _.bind(function(e){
-				$(e.currentTarget).find('.actions').hide();
-			}, this));
+			$('.backBtn').on('click', function(){
+				$('.labelDetails').fadeOut(function(){
+					$('.labels').fadeIn();
+				});
+			});
 			this.getUserData();
 		},
 		qwikAppData : {
@@ -30,35 +30,73 @@
 			labelDetails: {},
 			deletedLabelDetails: {}
 		},
+		getUserData : function() {
+			this.syncChromeStorage('get', 'qwikAppData', _.bind(function(data) {
+				if( ! _.isEmpty(data)) {
+					var storedData = _.has(data, 'qwikAppData') ? JSON.parse(data.qwikAppData) : {};
+					_.each(storedData, function(v, i){
+						if(_.has(this.qwikAppData, i)) {
+							this.qwikAppData[i] = v;
+						}
+					}, this);
+				} else if(_.has(this, 'qwikAppData')) {
+					this.syncChromeStorage('set', 'qwikAppData', JSON.stringify(this.qwikAppData));
+				}
+				this.showLabels();
+			}, this));
+		},
 		showLabels : function() {
 			var labelsList = $('.labelsList');
 			labelsList.html('');
 			if(_.has(this.qwikAppData, 'labels') && _.size(this.qwikAppData.labels) > 0) {
-				$('.noLabels').addClass('hidden');
 				_.each(this.qwikAppData.labels, function(v, i){
 					var id = v.id;
 					var name = v.name;
 					var li = this.creatElement('li', {class: 'label'});
 					var labelText = this.creatElement('div', {class: 'labelText fl m-top-3', 'data-id': id}, name);
 					var actions = this.creatElement('div', {class: 'actions fr', 'style': 'display:none'});
-					var editIcon = this.creatElement('div', {class: 'edit btn fa fw fa-pencil fl'});
-					var deleteIcon = this.creatElement('div', {class: 'delete btn error fa fw fa-trash fl', 'style': 'margin-left:10px'});
+					var editIcon = this.creatElement('div', {class: 'edit btn fa fw fa-pencil fl', 'data-id': id});
+					var deleteIcon = this.creatElement('div', {class: 'delete btn error fa fw fa-trash fl', 'data-id': id, 'style': 'margin-left:10px'});
 					actions.append(editIcon, deleteIcon, this.creatElement('div', {class: 'clearfix'}));
 					li.append(labelText, actions, this.creatElement('div', {class: 'clearfix'}));
-					/*<li class="label">
-						<div class="fl labelText m-top-3">Label 3</div>
-							<div class="fr actions" style="display:none">
-								<div class="fl edit btn fa fa-pencil fw"></div>
-									<div class="fl delete btn error fa fa-trash fw" style="margin-left:10px"></div>
-										<div class="clearfix"></div>
-											</div>
-											<div class="clearfix"></div>
-												</li>*/
+					labelsList.append(li);
 				}, this);
+				this.attachActionEvents($('.label'));
+				$('.label').on('click', '.labelText', _.bind(function(e) {
+					e.preventDefault();
+					var label = _.findWhere(this.qwikAppData.labels, {'id': Number($(e.currentTarget).attr('data-id'))});
+					if( ! _.isUndefined(label)) {
+						$('.labels').fadeOut(_.bind(function(){
+							this.showLabelDetails(label);
+						}, this));
+					}
+				}, this)).on('click', '.edit', _.bind(function(e){
+					e.preventDefault();
+					var label = _.findWhere(this.qwikAppData.labels, {'id': Number($(e.currentTarget).attr('data-id'))});
+					if( ! _.isUndefined(label)) {
+						$('.labels').fadeOut(_.bind(function(){
+							this.editLabel(label);
+						}, this));
+					}
+				}, this)).on('click', '.delete', _.bind(function(e){
+					e.preventDefault();
+					var cTarget = $(e.currentTarget);
+					var currentLabel = cTarget.parents('li');
+					if(confirm('Do you want to delete ' + currentLabel.find('.labelText').text() + '?')) {
+						var labels = _.filter(this.qwikAppData.labels, function(label){
+							return label.id !== Number(cTarget.attr('data-id'));
+						});
+						this.qwikAppData.labels = labels;
+						this.setQwikAppData();
+						currentLabel.remove();
+						if(labelsList.find('li').length <= 0) {
+							labelsList.append(this.creatElement('li', {class: 'no-label'}, 'Oops! Labels not available.'));
+						}
+					}
+				}, this));
 			} else {
-				$('.noLabels').removeClass('hidden');
+				labelsList.append(this.creatElement('li', {class: 'no-label'}, 'Oops! Labels not available.'));
 			}
-
 		},
 		addLabel : function() {
 			var addLabelForm = $('.addLabelForm');
@@ -68,39 +106,147 @@
 			addLabelForm.append(addLabelHeading);
 			/* Name */
 			var field1 = this.creatElement('div', {class: 'field'});
-			var label1 = this.creatElement('label', {for: 'labelName'}, 'Name');
-			var input1 = this.creatElement('input', {type: 'text', name: 'labelName', id: 'labelName', value: '', autocomplete: 'off'});
+			var label1 = '';//this.creatElement('label', {for: 'labelName'}, 'Name');
+			var input1 = this.creatElement('input', {type: 'text', name: 'labelName', id: 'labelName', value: '', autocomplete: 'off', placeholder: 'Name'});
 			field1.append(label1, input1);
 			/* Is Visible */
-			var field2 = this.creatElement('div', {class: 'field'});
+			/*var field2 = this.creatElement('div', {class: 'field'});
 			var label2 = this.creatElement('label', {for: 'labelIsVisible'}, 'Is Visible');
 			var input2 = this.creatElement('input', {type: 'checkbox', name: 'labelIsVisible', id: 'labelIsVisible'});
-			field2.append(label2, input2);
+			field2.append(label2, input2);*/
 			/* Is Disabled */
-			var field3 = this.creatElement('div', {class: 'field'});
+			/*var field3 = this.creatElement('div', {class: 'field'});
 			var label3 = this.creatElement('label', {for: 'labelIsDisabled'}, 'Is Disabled');
 			var input3 = this.creatElement('input', {type: 'checkbox', name: 'labelIsDisabled', id: 'labelIsDisabled'});
-			field3.append(label3, input3);
+			field3.append(label3, input3);*/
 			/* Position */
-			var field4 = this.creatElement('div', {class: 'field'});
-			var label4 = this.creatElement('label', {for: 'labelPosition'}, 'Position');
-			var input4 = this.creatElement('input', {type: 'text', name: 'labelPosition', id: 'labelPosition', value: '', autocomplete: 'off'});
-			field4.append(label4, input4);
+			/*var field4 = this.creatElement('div', {class: 'field'});
+			var label4 = '';//this.creatElement('label', {for: 'labelPosition'}, 'Position');
+			var input4 = this.creatElement('input', {type: 'number', name: 'labelPosition', id: 'labelPosition', value: '0', autocomplete: 'off', placeholder: 'Position', step: 1, min: 0, max: 99, maxlenth: 2});
+			field4.append(label4, input4);*/
 			/* action buttons */
 			var field5 = this.creatElement('div', {class: 'field'});
 			var save = this.creatElement('button', {name: 'addLabelSave', 'id': 'addLabelSave', class: 'fl btn success'}, 'Save');
 			var cancel = this.creatElement('button', {name: 'addLabelCancel', 'id': 'addLabelCancel', class: 'fl btn error', style: 'margin-left:10px'}, 'Cancel');
 			field5.append(save, cancel, this.creatElement('div', {class: 'clearfix'}));
-			addLabelForm.append(field1, field2, field3, field4, field5);
-			addLabelForm.find('#addLabelSave').on('click', function() {
-				alert('add label save clicked!');
-			});
-			addLabelForm.find('#addLabelCancel').on('click', _.bind(function() {
-				addLabelForm.fadeOut(function() {
-					$('.labels').fadeIn();
+			addLabelForm.append(field1, /*field2, field3, field4,*/ field5);
+			addLabelForm.find('#addLabelSave').on('click', _.bind(function() {
+				var labelName = $.trim($('#labelName').val());
+				var filteredData = _.filter(this.qwikAppData.labels, function(label){
+					return labelName === label.name;
 				});
+				if(_.size(filteredData) <= 0){
+					var lastLabel = _.last(this.qwikAppData.labels);
+					this.qwikAppData.labels.push({
+						id: ! _.isUndefined(lastLabel) ? Number(lastLabel.id) + 1 : 1,
+						name: labelName,
+						position: 0,
+						isVisible: 1,
+						isDisabled: 0
+					});
+					this.setQwikAppData();
+					addLabelForm.fadeOut(_.bind(function(){
+						$('.labelsList').html(this.creatElement('li', {class: 'no-label'}, 'Loading labels... Please Wait...'));
+						$('.labels').fadeIn();
+						this.showLabels();
+					}, this));
+				} else {
+					$('#addLabelCancel').trigger('click');
+				}
 			}, this));
-			$('.addLabelForm').fadeIn();
+			addLabelForm.find('#addLabelCancel').on('click', _.bind(function() {
+				addLabelForm.fadeOut(_.bind(function() {
+					$('.labelsList').html(this.creatElement('li', {class: 'no-label'}, 'Loading labels... Please Wait...'));
+					$('.labels').fadeIn();
+					this.showLabels();
+				}, this));
+			}, this));
+			addLabelForm.fadeIn();
+		},
+		editLabel : function(label) {
+			var addLabelForm = $('.addLabelForm');
+			/* clean div content before appding elements */
+			addLabelForm.html('');
+			var addLabelHeading = this.creatElement('div', {class: 'labelHeading'}, 'Edit Label');
+			addLabelForm.append(addLabelHeading);
+			/* Name */
+			var field1 = this.creatElement('div', {class: 'field'});
+			var label1 = '';//this.creatElement('label', {for: 'labelName'}, 'Name');
+			var input1 = this.creatElement('input', {type: 'text', name: 'labelName', id: 'labelName', value: label.name, autocomplete: 'off', placeholder: 'Name'});
+			field1.append(label1, input1);
+			/* Is Visible */
+			/*var field2 = this.creatElement('div', {class: 'field'});
+			var label2 = this.creatElement('label', {for: 'labelIsVisible'}, 'Is Visible');
+			var input2 = this.creatElement('input', {type: 'checkbox', name: 'labelIsVisible', id: 'labelIsVisible'});
+			field2.append(label2, input2);*/
+			/* Is Disabled */
+			/*var field3 = this.creatElement('div', {class: 'field'});
+			var label3 = this.creatElement('label', {for: 'labelIsDisabled'}, 'Is Disabled');
+			var input3 = this.creatElement('input', {type: 'checkbox', name: 'labelIsDisabled', id: 'labelIsDisabled'});
+			field3.append(label3, input3);*/
+			/* Position */
+			/*var field4 = this.creatElement('div', {class: 'field'});
+			var label4 = '';//this.creatElement('label', {for: 'labelPosition'}, 'Position');
+			var input4 = this.creatElement('input', {type: 'number', name: 'labelPosition', id: 'labelPosition', value: '0', autocomplete: 'off', placeholder: 'Position', step: 1, min: 0, max: 99, maxlenth: 2});
+			field4.append(label4, input4);*/
+			/* action buttons */
+			var field5 = this.creatElement('div', {class: 'field'});
+			var save = this.creatElement('button', {name: 'editLabelSave', 'id': 'editLabelSave', class: 'fl btn success'}, 'Save');
+			var cancel = this.creatElement('button', {name: 'editLabelCancel', 'id': 'editLabelCancel', class: 'fl btn error', style: 'margin-left:10px'}, 'Cancel');
+			field5.append(save, cancel, this.creatElement('div', {class: 'clearfix'}));
+			addLabelForm.append(field1, /*field2, field3, field4,*/ field5);
+			addLabelForm.find('#editLabelSave').on('click', _.bind(function() {
+				var labelName = $.trim($('#labelName').val());
+				var duplicateLabel = _.findIndex(this.qwikAppData.labels, function(labelData){
+					return label.id !== labelData.id && label.name === labelData.name;
+				});
+				var labelIndex = _.findIndex(this.qwikAppData.labels, function(labelData){
+					return label.id === labelData.id;
+				});
+				if(label.name !== labelName && duplicateLabel === -1 && labelIndex > -1) {
+					this.qwikAppData.labels[labelIndex].name = labelName;
+					this.setQwikAppData();
+					addLabelForm.fadeOut(_.bind(function(){
+						$('.labelsList').html(this.creatElement('li', {class: 'no-label'}, 'Loading labels... Please Wait...'));
+						$('.labels').fadeIn();
+						this.showLabels();
+					}, this));
+				} else {
+					$('#editLabelCancel').trigger('click');
+				}
+			}, this));
+			addLabelForm.find('#editLabelCancel').on('click', _.bind(function() {
+				addLabelForm.fadeOut(_.bind(function() {
+					$('.labelsList').html(this.creatElement('li', {class: 'no-label'}, 'Loading labels... Please Wait...'));
+					$('.labels').fadeIn();
+					this.showLabels();
+				}, this));
+			}, this));
+			addLabelForm.fadeIn();
+		},
+		showLabelDetails : function(labelId, labelName) {
+			var labelDetailsList = $('.labelDetailsList');
+			$('.labelDetailHeading').html('').text(labelName);
+			labelDetailsList.html('');
+			if(_.has(this.qwikAppData, 'labelDetails') && _.has(this.qwikAppData.labelDetails, labelName) && _.size(this.qwikAppData.labelDetails[labelName]) > 0) {
+				_.each(this.qwikAppData.labelDetails[labelName], function(v, i){
+					var id = v.id;
+					var name = v.name;
+					var li = this.creatElement('li', {class: 'labelDetail'});
+					var labelText = this.creatElement('div', {class: 'labelText fl m-top-3', 'data-id': id}, name);
+					var actions = this.creatElement('div', {class: 'actions fr', 'style': 'display:none'});
+					var editIcon = this.creatElement('div', {class: 'edit btn fa fw fa-pencil fl'});
+					var deleteIcon = this.creatElement('div', {class: 'delete btn error fa fw fa-trash fl', 'style': 'margin-left:10px'});
+					actions.append(editIcon, deleteIcon, this.creatElement('div', {class: 'clearfix'}));
+					li.append(labelText, actions, this.creatElement('div', {class: 'clearfix'}));
+					labelDetailsList.append(li);
+				}, this);
+			} else {
+				var li = this.creatElement('li', {class: 'no-label'}, 'Oops! Label Details not available.');
+				labelDetailsList.append(li);
+			}
+			this.attachActionEvents($('.labelDetail'));
+			$('.labelDetails').fadeIn();
 		},
 		addLabelDetail: function () {
 			var addLabelDetailForm = $('.addLabelDetailForm');
@@ -160,21 +306,6 @@
 				});
 			}, this));
 		},
-		getUserData : function() {
-			chrome.storage.sync.get('qwikAppData', _.bind(function(data) {
-				if( ! _.isEmpty(data)) {
-					var storedData = _.has(data, 'qwikAppData') ? JSON.parse(data.qwikAppData) : {};
-					_.each(storedData, function(v, i){
-						if(_.has(this.qwikAppData, i)) {
-							this.qwikAppData[i] = v;
-						}
-					}, this);
-					this.showLabels();
-				} else if(_.has(this, 'qwikAppData')) {
-					chrome.storage.sync.set({'qwikAppData': JSON.stringify(this.qwikAppData)});
-				}
-			}, this));
-		},
 		creatElement: function(str, params, data) {
 			var elem = null;
 			if(_.isString(str) && ! _.isEmpty(str)) {
@@ -184,7 +315,30 @@
 				}
 			}
 			return elem;
+		},
+		attachActionEvents : function(selector) {
+			$(selector).on('mouseenter', function(e){
+				$(e.currentTarget).find('.actions').show();
+			}).on('mouseleave', function(e){
+				$(e.currentTarget).find('.actions').hide();
+			});
+		},
+		setQwikAppData: function() {
+			this.syncChromeStorage('set', 'qwikAppData', JSON.stringify(this.qwikAppData));
+		},
+		syncChromeStorage: function(type, key, value) {
+			if(_.isString(type) && ! _.isEmpty(type)) {
+				if(type === 'set') {
+					var obj = {};
+					obj[key] = value;
+					chrome.storage.sync.set(obj);
+				} else if(type === 'get' && _.isFunction(value)) {
+					chrome.storage.sync.get(key, value)
+				}
+			}
 		}
 	};
-	qwikApp.init();
+	document.addEventListener("DOMContentLoaded", function() {
+		qwikApp.init();
+	});
 }(window.jQuery));
---
+++ scripts/app/bg.js
@@ -0,0 +1,3 @@
+$(function(){
+	console.log('background script called!! ', new Date());
+});
